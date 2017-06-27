cd9428dbf3c8891adadccaf7204da2cd37164672
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2017-06-27 11:47:59 -0300

    Remove temp artifacty

--- Sample01/Main2.java
+++
@@ -1,7 +0,0 @@
-public class Main2 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Advanced Java World\n");
-        System.out.println("Hello from the second method\n");
-    }
-}
