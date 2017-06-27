3cb561df805f6b89d6b2fb83c4c4602352324f64
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2017-06-27 11:44:02 -0300

    Ajustes

--- Sample01/linuxC.sh
+++ Sample01/linuxC.sh
@@ -5,21 +5,20 @@ JNI_INCLUDE="-I/usr/lib64/jdk1.8.0_65/include -I/usr/lib64/jdk1.8.0_65/include/l
 export LD_LIBRARY_PATH=.
 echo "======================================================"
 echo "Compiling Java files..."
-time javac NativeMethods.java Main.java Main2.java
+javac NativeMethods.java Main.java
 echo "Java files compiled"
 
 echo "Generatinh header files..."
-time javah -jni NativeMethods
+javah -jni NativeMethods
 echo "Header file created"
 
 echo "Generating native shared library..."
-time gcc $JNI_INCLUDE -fPIC -shared cImplOne.c cImplTwo.c -o libNativeLib.so
+gcc $JNI_INCLUDE -fPIC -shared cImplOne.c cImplTwo.c -o libNativeLib.so
 echo "Shared library created"
 echo "======================================================"
 
 echo "Testing App..."
 echo ">>> Output <<<"
-time java Main
-time java Main2
+java Main
 echo "======================================================"
 echo
